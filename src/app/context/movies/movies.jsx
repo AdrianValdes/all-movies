f8a241f52a06f23230d784b5392087ae21a381ae
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-02-10 21:55:53 +0100

    refactor: delelte unused vars

--- src/app/context/movies/movies.jsx
+++ src/app/context/movies/movies.jsx
@@ -1,10 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { useFetch } from '../../hooks/useFetch';
 
 const MoviesContext = createContext();
