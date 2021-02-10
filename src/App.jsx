2039b83ae503baf7140ea10b3da8b3ae64a24737
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-02-10 19:17:29 +0100

    feat: :zap: fetch data from the API
    Also refactor some components, add more exceptions to the eslint rules, added css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MoviesProvider } from './app/context/movies/movies';
 import { Footer } from './app/Footer';
+
 import { Navbar } from './app/Navbar';
 import { Example } from './features/example/Example';
+import { MoviesDashboard } from './features/movies/MoviesDashboard';
 
 export const App = () => (
   <Router>
     <Navbar />
     <Switch>
+      <MoviesProvider>
+        <Route>
+          <MoviesDashboard />
+        </Route>
+      </MoviesProvider>
       <Route exact path='/'>
         <Example />
       </Route>
