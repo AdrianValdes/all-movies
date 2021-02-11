031cc657ffb70ee48d6f279d461bb350eb3f6503
Joyce Sosaya <67947482+Winkel2019@users.noreply.github.com>
2021-02-11 21:28:28 +0100

    add Header (#15)
    * add Header
    
    * working on dropdown
    
    * add dropdown
    
    * add header and style footer
    
    * install react icons
    
    * install react redux
    
    Co-authored-by: AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import { Footer } from './app/Footer';
 import { MovieRoute } from './app/MovieRoute';
 
 import { Navbar } from './app/Navbar';
+import { HomeBanner } from './app/HomeBanner';
+// import { PageBanner } from './app/PageBanner';
 import { MoviesDashboard } from './features/movies/MoviesDashboard';
 
 export const App = () => (
   <Router>
     <Navbar />
     <Switch>
+      <HomeBanner />
+      {/* <PageBanner /> */}
       <Route path='/movie/:id' component={MovieRoute} />
-
       <Route exact path='/'>
         <MoviesDashboard />
       </Route>
--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,7 +1,111 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable react/jsx-indent */
+import { Link } from 'react-router-dom';
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../assets/images/logo.png';
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: rgb(3, 37, 65);
+  display: flex;
+  padding: 4px 20px;
+`;
+
+const StyledLi = styled.li`
+  padding: 10px;
+  float: left;
+`;
+
+const Dropbtn = styled.div`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  &:hover {
+    color: lightseagreen;
+  }
+`;
+
+const DropDownContent = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+`;
+
+const DropDownLi = styled(StyledLi)`
+  display: inline-block;
+  font-size: 20px;
+  &:hover ${DropDownContent} {
+    display: block;
+  }
+`;
+
+const StyledA = styled.a`
+  display: inline-block;
+  color: white;
+  font-size: 20px;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  &:hover {
+    color: lightseagreen;
+  }
+`;
+
+const SubA = styled.a`
+  color: rgb(3, 37, 65);
+  padding: 10px 12px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+  &:hover {
+    color: white;
+    background-color: rgba(3, 37, 65, 0.7);
+  }
+`;
+
+const LogoStyle = styled.img`
+  width: 100px;
+`;
 
 export const Navbar = () => (
-  <div>
-    <h2>This is the Navbar</h2>
-  </div>
+  <StyledUl>
+    <Link to='/'>
+      <LogoStyle src={logo} alt='logo' />
+    </Link>
+    <StyledLi>
+      <Link to='/popular'>
+        <StyledA>Popular</StyledA>
+      </Link>
+    </StyledLi>
+    <DropDownLi>
+      <Dropbtn>Genres</Dropbtn>
+      <DropDownContent>
+        {' '}
+        <SubA>Action</SubA>
+        <SubA>Animation</SubA>
+        <SubA>Comedy</SubA>
+        <SubA>Drama</SubA>
+      </DropDownContent>
+    </DropDownLi>
+    <StyledLi>
+      <Link to='/people'>
+        <StyledA>People</StyledA>
+      </Link>
+    </StyledLi>
+    <StyledLi>
+      <Link to='/login'>
+        <StyledA>Login</StyledA>
+      </Link>
+    </StyledLi>
+  </StyledUl>
 );
