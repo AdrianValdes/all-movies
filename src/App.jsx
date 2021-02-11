a27218df757ba13d5a6d618836ff99858a6ca8b0
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-02-11 09:05:23 +0100

    Created use fetch hook (#14)
    * feat: :zap: fetch data from the API
    
    Also refactor some components, add more exceptions to the eslint rules, added css.
    
    * feat: :art: add useFetch custom hook. Add more components to main
    
    * refactor: delelte unused vars
    
    * refactor: remove more unused vars
    
    * refactor: delete unnecessary component `Example`
    
    * fix: :bug: delete component from the `imports`

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { MoviesProvider } from './app/context/movies/movies';
 import { Footer } from './app/Footer';
 
 import { Navbar } from './app/Navbar';
-import { Example } from './features/example/Example';
 import { MoviesDashboard } from './features/movies/MoviesDashboard';
 
 export const App = () => (
@@ -12,13 +11,10 @@ export const App = () => (
     <Navbar />
     <Switch>
       <MoviesProvider>
-        <Route>
+        <Route exact path='/'>
           <MoviesDashboard />
         </Route>
       </MoviesProvider>
-      <Route exact path='/'>
-        <Example />
-      </Route>
     </Switch>
     <Footer />
   </Router>
--- src/app/context/movies/movies.jsx
+++ src/app/context/movies/movies.jsx
@@ -1,32 +1,36 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
+import { useFetch } from '../../hooks/useFetch';
 
 const MoviesContext = createContext();
 export const useMovies = () => useContext(MoviesContext);
 
 const KEY = process.env.REACT_APP_KEY;
-const popularity =
-  'https://api.themoviedb.org/3/discover/movie?api_key=009f9976a57d0b92e9dee06122c5b4bc&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1';
 
 const genres =
   'https://api.themoviedb.org/3/genre/movie/list?api_key=009f9976a57d0b92e9dee06122c5b4bc&language=en-US';
 
 export const MoviesProvider = ({ children }) => {
   const [comedies, setComedies] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [animation, setAnimation] = useState([]);
 
-  useEffect(() => {
-    async function fetchComedies() {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&with_genres=35&page=1`
-      );
-      const { results } = await response.json();
-      setComedies((prevState) => [...prevState, ...results]);
-    }
+  const { loading: loadingComedies, error: errorComedies } = useFetch({
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&with_genres=35&page=1`,
+    setMovies: setComedies,
+  });
 
-    fetchComedies();
-  }, []);
+  const { loading: loadingPopularity, error: errorPopularity } = useFetch({
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`,
+    setMovies: setPopular,
+  });
+
+  const { loading: loadingAnimation, error: errorAnimation } = useFetch({
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&with_genres=16&page=1`,
+    setMovies: setAnimation,
+  });
 
   return (
-    <MoviesContext.Provider value={{ comedies }}>
+    <MoviesContext.Provider value={{ comedies, popular, animation }}>
       {children}
     </MoviesContext.Provider>
   );
