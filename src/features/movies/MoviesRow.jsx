68751d8351156482628e7e134c1d6f5001acaede
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-02-11 15:57:05 +0100

    feat: :zap: add redux, delete context
    also add a route for each movie

--- src/features/movies/MoviesRow.jsx
+++ src/features/movies/MoviesRow.jsx
@@ -15,19 +15,21 @@ const Row = styled.div`
 
 export const MoviesRow = ({ movies, moviesError }) => (
   <Row>
-    {moviesError ? (
-      <MoviesError moviesError={moviesError} />
-    ) : (
-      movies.map(({ poster_path, release_date, vote_average, title, id }) => (
-        <Link to={`/movie/${id}`} key={id}>
-          <MovieCard
-            poster_path={poster_path}
-            release_date={release_date}
-            vote_average={vote_average}
-            title={title}
-          />
-        </Link>
-      ))
-    )}
+    {movies.map(({ poster_path, release_date, vote_average, title, id }) => (
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: { poster_path, release_date, title },
+        }}
+        key={id}
+      >
+        <MovieCard
+          poster_path={poster_path}
+          release_date={release_date}
+          vote_average={vote_average}
+          title={title}
+        />
+      </Link>
+    ))}
   </Row>
 );
