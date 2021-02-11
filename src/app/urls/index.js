1e32d90a22338c7af9e96d54a2042f44fbf26ecd
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-02-11 21:35:03 +0100

    add actors to movie route, add trailer, add Home component, add random image for Banner

--- src/app/urls/index.js
+++ src/app/urls/index.js
@@ -1,4 +1,8 @@
 export const KEY = process.env.REACT_APP_KEY;
+export const IMAGE_BASE_URL_LOW = 'https://image.tmdb.org/t/p/w185';
+export const IMAGE_BASE_URL_MEDIUM = 'https://image.tmdb.org/t/p/w500';
+export const IMAGE_BASE_URL_HIGH = 'https://image.tmdb.org/t/p/w1280';
+export const SINGLE_MOVIE_BASE_URL = 'https://api.themoviedb.org/3/movie';
 
 export const genres =
   'https://api.themoviedb.org/3/genre/movie/list?api_key=009f9976a57d0b92e9dee06122c5b4bc&language=en-US';
