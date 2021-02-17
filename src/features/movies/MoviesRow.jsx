f4582d98d143943df4168f08b19d5ce5eb58faef
AdrianValdes <58083933+AdrianValdes@users.noreply.github.com>
2021-02-17 14:18:56 +0100

    add fading to scrollbar (#40)

--- src/features/movies/MoviesRow.jsx
+++ src/features/movies/MoviesRow.jsx
@@ -9,7 +9,22 @@ export const Row = styled.div`
   overflow-y: hidden;
   padding-top: 20px;
   padding-bottom: 20px;
-
+  position: relative;
+  &::after {
+    content: '';
+    width: 60px;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-image: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0) 0%,
+      #fff 100%
+    );
+    will-change: opacity;
+    pointer-events: none;
+  }
   &::-webkit-scrollbar {
     height: 8px;
     background-color: rgba(219, 219, 219);
