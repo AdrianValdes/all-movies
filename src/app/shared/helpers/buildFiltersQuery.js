6cee495079c9642575d7addb131e385f2b591074
Adrian Valdes <adrianvaldeshdz@gmail.com>
2021-02-19 13:50:31 +0100

    fix bug on filtering functionality

--- src/app/shared/helpers/buildFiltersQuery.js
+++ src/app/shared/helpers/buildFiltersQuery.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/no-cycle */
+import { popularUrl, showsUrl } from '../urls';
+
 const baseStringCount = 99;
+
 export const buildFiltersQuery = ({
   genreUrl,
   sort,
   score,
   language,
-  original_language = 'en-US',
+  original_language,
 }) =>
-  `${genreUrl.slice(
-    0,
-    baseStringCount
-  )}&${sort}&${score}&language=${language}&with_original_language=${original_language}`;
+  `${genreUrl}&${sort}&${score}&language=${language}&with_original_language=${original_language}`;
